2ccd1335170dee5907d6fdfeec718405cd147528
Lukas Meyer <lf.meyer96@gmail.com>
2016-12-21 22:23:29 +0100

    add inventory

---
+++ Assets/Rohstoff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Rohstoff {
+
+	public string name;
+	public int id;
+	public Texture2D aussehen;
+
+	public Rohstoff(string itemName, int itemID) {
+		name = itemName;
+		id = itemID;
+		aussehen = Resources.Load<Texture2D> (itemName);
+	}
+
+	public Rohstoff() {
+	}
+
+}
---
+++ Assets/RohstoffListe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RohstoffListe : MonoBehaviour {
+
+	public List<Rohstoff> materialien = new List<Rohstoff>();
+
+	void Start() 
+	{
+		int i = 0;
+		materialien.Add (new Rohstoff("Holz", i++));
+		materialien.Add (new Rohstoff("Nagel", i++));
+		materialien.Add (new Rohstoff("Schraube", i++));
+	}
+
+}
