6531e49d5bb55ec240e04dcbb1b7f815642e3774
Lukas Meyer <lf.meyer96@gmail.com>
2016-12-22 03:05:23 +0100

    implement item switching

---
+++ Assets/Inventare.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventare : MonoBehaviour {
+
+	public int breiteQuelleInventar;
+	public int hoeheQuelleInventar;
+	public int breiteTransportmittelInventar;
+	public int hoeheTransportmittelInventar;
+	public GUISkin skin;
+	public List<Rohstoff> lagerInventar = new List<Rohstoff> ();
+	public List<Rohstoff> transportmittelInventar = new List<Rohstoff> ();
+	private List<Rohstoff> slotsQuelle = new List<Rohstoff> ();
+	private List<Rohstoff> slotsTransportmittel = new List<Rohstoff> ();
+	private RohstoffListe rohstoffListe;
+	private bool istSichtbar = false;
+	private float lastTime;
+	private const float LANGER_KLICK = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < breiteQuelleInventar * hoeheQuelleInventar; i++) {
+			slotsQuelle.Add (new Rohstoff());
+			lagerInventar.Add (new Rohstoff ());
+		}
+
+		for(int i = 0; i < breiteTransportmittelInventar*hoeheTransportmittelInventar; i++) {
+			slotsTransportmittel.Add (new Rohstoff());
+			transportmittelInventar.Add (new Rohstoff());
+		}
+		print (transportmittelInventar.Count);
+
+		rohstoffListe = GameObject.FindGameObjectWithTag ("Rohstoff Liste").GetComponent<RohstoffListe>();
+
+		RohstoffQuelleHinzufuegen (0);
+		RohstoffQuelleHinzufuegen (1);
+		RohstoffQuelleHinzufuegen (2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		lastTime = Time.time;
+	}
+
+	void OnMouseUp() {
+		if (Time.time - lastTime >= LANGER_KLICK) {
+			istSichtbar = !istSichtbar;
+		}
+
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin = skin;
+		if(istSichtbar) {
+			ZeigeInventare ();
+		}
+	}
+
+	void ZeigeInventare() {
+		Event e = Event.current;
+		ZeigeQuellenInventar (e);
+		ZeigeTransportmittelInventar (e);
+	}
+
+	void ZeigeQuellenInventar(Event e) {
+		int i = 0;
+		for(int x = 0; x < breiteQuelleInventar; x++) {
+			for(int y = 0; y < hoeheQuelleInventar; y++) {
+				Rect slotRect = new Rect (x * 50, y*50, 50, 50);
+				GUI.Box (new Rect(x * 50, y * 50, 50, 50), "", skin.GetStyle("Slot"));
+				slotsQuelle [i] = lagerInventar [i];
+				if(slotsQuelle[i].name != null) {
+					GUI.DrawTexture (slotRect, slotsQuelle[i].aussehen);
+					if(slotRect.Contains(e.mousePosition)) {
+						if(e.button == 0 && e.type == EventType.mouseDown) {
+							Rohstoff ausgewaehlterRohstoff = slotsQuelle [i];
+							RohstoffVonQuelleEntfernen (ausgewaehlterRohstoff.id);
+							RohstoffTransportmittelHinzufuegen (ausgewaehlterRohstoff.id);
+						}
+					}
+				}
+				i++;
+			}
+		}
+	}
+
+	void RohstoffQuelleHinzufuegen(int itemID) {
+		for(int i = 0; i < lagerInventar.Count; i++) {
+			if(lagerInventar[i].name == null) {
+
+				for(int j = 0; j < rohstoffListe.materialien.Count; j++) {
+					if(rohstoffListe.materialien[j].id == itemID) {
+						lagerInventar [i] = rohstoffListe.materialien [j];
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	void RohstoffVonQuelleEntfernen(int itemID) {
+		for(int i = 0; i < lagerInventar.Count; i++) {
+			if(lagerInventar[i].id == itemID) {
+				lagerInventar [i] = new Rohstoff ();
+				break;
+			}
+		}
+	}
+
+	void RohstoffVonTransportmittelEntfernen(int itemID) {
+		for(int i = 0; i < transportmittelInventar.Count; i++) {
+			if(transportmittelInventar[i].id == itemID) {
+				transportmittelInventar [i] = new Rohstoff ();
+				break;
+			}
+		}
+	}
+
+	void RohstoffTransportmittelHinzufuegen(int itemID) {
+		for(int i = 0; i < transportmittelInventar.Count; i++) {
+			if(transportmittelInventar[i].name == null) {
+
+				for(int j = 0; j < rohstoffListe.materialien.Count; j++) {
+					if(rohstoffListe.materialien[j].id == itemID) {
+						transportmittelInventar [i] = rohstoffListe.materialien [j];
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	void ZeigeTransportmittelInventar(Event e) {
+		int i = 0;
+		for(int x = 0; x < breiteTransportmittelInventar; x++) {
+			for(int y = 0; y < hoeheTransportmittelInventar; y++) {
+				Rect slotRect = new Rect (x * 50, y*50 + (hoeheQuelleInventar+2)*50, 50, 50);
+				GUI.Box (new Rect(x * 50, y * 50 + (hoeheQuelleInventar + 2)*50, 50, 50), "", skin.GetStyle("Slot"));
+				slotsTransportmittel [i] = transportmittelInventar [i];
+				if(slotsTransportmittel[i].name != null) {
+					GUI.DrawTexture (slotRect, slotsTransportmittel[i].aussehen);
+				}
+				i++;
+			}
+		}
+	}
+}
--- Assets/Senke_03.cs
+++ Assets/Senke_03.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Senke_03 : MonoBehaviour
 {
+
+	public int breiteSenkeInventar;
+	public int hoeheSenkeInventar;
+	public int breiteTransportmittelInventar;
+	public int hoeheTransportmittelInventar;
+	public GUISkin skin;
+	public List<Rohstoff> lagerInventar = new List<Rohstoff> ();
+	public List<Rohstoff> transportmittelInventar = new List<Rohstoff> ();
+	private List<Rohstoff> slotsSenke = new List<Rohstoff> ();
+	private List<Rohstoff> slotsTransportmittel = new List<Rohstoff> ();
+	private RohstoffListe rohstoffListe;
+	private bool istSichtbar = false;
+	private float lastTime;
+	private const float LANGER_KLICK = 0.5f;
 
     public GameObject Quelle_01_Senke_03;
     public GameObject Quelle_02_Senke_03;
@@ -21,29 +36,40 @@ public class Senke_03 : MonoBehaviour
 
     }
 
-    void OnMouseDown()
-    {
-        Renderer rend = GetComponent<Renderer>();
-        Renderer rendQ1S3 = Quelle_01_Senke_03.GetComponent<Renderer>();
-        Renderer rendQ2S3 = Quelle_02_Senke_03.GetComponent<Renderer>();
-        Renderer rendQ3S3 = Quelle_03_Senke_03.GetComponent<Renderer>();
-        //rend2.enabled = true;
-        //rend3.enabled = true;
-        if (!active)
-        {
-            rend.material.color = new Color(0, 255, 0);
-            rendQ1S3.material.color = new Color(255, 0, 0);
-            rendQ2S3.material.color = new Color(255, 0, 0);
-            rendQ3S3.material.color = new Color(255, 0, 0);
-            active = true;
-        }
-        else if (active)
-        {
-            rend.material.color = Color.black;
-            rendQ1S3.material.color = Color.black;
-            rendQ2S3.material.color = Color.black;
-            rendQ3S3.material.color = Color.black;
-            active = false;
-        }
-    }
+	void OnMouseDown() {
+		lastTime = Time.time;
+	}
+
+	void OnMouseUp() {
+		
+			if (Time.time - lastTime >= LANGER_KLICK) {
+				istSichtbar = !istSichtbar;
+			print ("stimmt genau so");
+			} else {
+
+				Renderer rend = GetComponent<Renderer>();
+				Renderer rendQ1S3 = Quelle_01_Senke_03.GetComponent<Renderer>();
+				Renderer rendQ2S3 = Quelle_02_Senke_03.GetComponent<Renderer>();
+				Renderer rendQ3S3 = Quelle_03_Senke_03.GetComponent<Renderer>();
+				//rend2.enabled = true;
+				//rend3.enabled = true;
+				if (!active)
+				{
+					rend.material.color = new Color(0, 255, 0);
+					rendQ1S3.material.color = new Color(255, 0, 0);
+					rendQ2S3.material.color = new Color(255, 0, 0);
+					rendQ3S3.material.color = new Color(255, 0, 0);
+					active = true;
+				}
+				else if (active)
+				{
+					rend.material.color = Color.black;
+					rendQ1S3.material.color = Color.black;
+					rendQ2S3.material.color = Color.black;
+					rendQ3S3.material.color = Color.black;
+					active = false;
+				}
+			}
+
+	}
 }
